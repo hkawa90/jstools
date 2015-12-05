3646ef60e13436a8a201847c62901360fe98529b
Kawamura Hideo <kawa90@yahoo.co.jp>
2015-12-05 10:55:33 +0900

    remove log

--- samples/file_listing.js
+++ samples/file_listing.js
@@ -1,25 +1,14 @@
 module.exports = function (base, filterEx, callback) {
     var fs = require('fs');
     var filter = filterEx || /.*\.js$/;
-    console.log(base);
-    console.log(filterEx);
     fs.readdir(base, function (err, files) {
-        console.log('---');
-        console.log(files);
-        console.log('---');
         if (err) throw err;
         var fileList = [];
         files.filter(function (file) {
-            console.log('---');
-            console.log(file);
-            console.log('---');
             return fs.statSync(file).isFile() && filter.test(file);
         }).forEach(function (file) {
             fileList.push(file);
         });
-        console.log('--');
-        console.log(fileList);
-        console.log('--');
         callback(null, fileList);
         return fileList;
     });
